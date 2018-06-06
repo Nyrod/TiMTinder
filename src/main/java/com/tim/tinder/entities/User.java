4b13b972bc7c60bc80137923b5ed35795b8c6315
Michał <fierkamichal@gmail.com>
2018-06-06 16:10:38 +0200

    a

--- src/main/java/com/tim/tinder/entities/User.java
+++ src/main/java/com/tim/tinder/entities/User.java
@@ -1,4 +1,13 @@
 package com.tim.tinder.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long idUser;
 }
