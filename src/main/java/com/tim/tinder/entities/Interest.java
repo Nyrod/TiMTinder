65622ed77094849dd1c66f4bd70b8398d97d5e22
Nyrod <pawfr876@gmail.com>
2018-06-06 16:29:40 +0200

    ent

--- src/main/java/com/tim/tinder/entities/Interest.java
+++ src/main/java/com/tim/tinder/entities/Interest.java
@@ -1,4 +1,24 @@
 package com.tim.tinder.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Interest {
+
+    @Id
+    @GeneratedValue
+    private Long idIntrest;
+
+    private String name;
 }
--- src/main/java/com/tim/tinder/entities/Photo.java
+++ src/main/java/com/tim/tinder/entities/Photo.java
@@ -1,4 +1,26 @@
 package com.tim.tinder.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Photo {
+
+    @Id
+    @GeneratedValue
+    private Long idPhoto;
+
+    @Lob
+    private byte[] photo;
 }
