d30db33f375bf1c909fe4dc0ba5af08f2c3db1de
Nyrod <pawfr876@gmail.com>
2018-06-06 13:15:11 +0200

    Initial commit

---
+++ src/main/java/com/tim/tinder/TinderApplication.java
@@ -0,0 +1,12 @@
+package com.tim.tinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderApplication.class, args);
+	}
+}
