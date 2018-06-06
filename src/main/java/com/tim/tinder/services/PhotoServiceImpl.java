04342db4df773b1ee3c24d1cd5a1a3835c06b475
Michał <fierkamichal@gmail.com>
2018-06-06 22:16:34 +0200

    a

--- src/main/java/com/tim/tinder/services/PhotoServiceImpl.java
+++ src/main/java/com/tim/tinder/services/PhotoServiceImpl.java
@@ -1,8 +1,42 @@
 package com.tim.tinder.services;
 
+import com.tim.tinder.TinderException;
+import com.tim.tinder.entities.Photo;
+import com.tim.tinder.entities.User;
+import com.tim.tinder.repositories.UserRepository;
 import com.tim.tinder.services.interfaces.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PhotoServiceImpl implements PhotoService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void addPhotoToUser(Long idUser, byte[] photo) {
+        User user = null;
+        try {
+            user = userRepository.findById(idUser).orElseThrow(() -> new TinderException("Nie znaleziono użytkownika o id " + idUser));
+        } catch (TinderException e) {
+            e.printStackTrace();
+        }
+        user.getPhotos().add(new Photo(photo));
+    }
+
+    @Override
+    public void changeUserAvatar(Long idUser, byte[] avatar) {
+
+    }
+
+    @Override
+    public byte[] getPhoto(Long idPhoto) {
+        return new byte[0];
+    }
+
+    @Override
+    public void deletePhoto(long idPhoto) {
+
+    }
 }
